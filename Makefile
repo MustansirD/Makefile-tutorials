bbde92deb11328db1f92ae381678dbc245579523
MustansirD <mustansirdaman@gmail.com>
2019-01-17 09:48:38 +0530

    First Commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+contents:=$(shell cat file.txt)
+
+all:
+	echo $(contents)
---
+++ temp/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"hello.h"
+#define pi 4
+void printfunc(void){
+	int i = pi;
+	printf("hello world %d\n",i);
+}
